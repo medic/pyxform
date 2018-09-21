a0fde1e7e86371d3de67bcdf6794c72721d92db0
Victor Gomez <victor@medicmobile.org>
2018-09-21 16:00:12 -0600

    Revert "Trim whitespace at start and end of labels"
    This reverts commit 8cde24320b7356213d934f8437dafe425ff352fe.

--- pyxform/tests_v1/test_whitespace.py
+++ pyxform/tests_v1/test_whitespace.py
@@ -1,32 +1,15 @@
 from pyxform.tests_v1.pyxform_test_case import PyxformTestCase
 
 class WhitespaceTest(PyxformTestCase):
-    def test_over_trim_before(self):
+    def test_over_trim(self):
         self.assertPyxformXform(
-            name='issue96_issue146',
-            ss_structure={'survey': [
-                    { 'type':'text', 'label':'Ignored',    'name':'var'   },
-                    { 'type':'note', 'label':'${var} end', 'name':'label' } ] },
+            name='issue96',
+            md="""
+            | survey  |                 |             |       |
+            |         | type            | label       | name  |
+            |         | text            | Ignored     | var   |
+            |         | note            | ${var} text | label |
+            """,
             xml__contains=[
-                '<label><output value=" /issue96_issue146/var "/> end</label>',
-            ])
-
-    def test_over_trim_middle(self):
-        self.assertPyxformXform(
-            name='issue96_issue146',
-            ss_structure={'survey': [
-                    { 'type':'text', 'label':'Ignored',          'name':'var'   },
-                    { 'type':'note', 'label':'start ${var} end', 'name':'label' } ] },
-            xml__contains=[
-                '<label>start <output value=" /issue96_issue146/var "/> end</label>',
-            ])
-
-    def test_over_trim_after(self):
-        self.assertPyxformXform(
-            name='issue96_issue146',
-            ss_structure={'survey': [
-                    { 'type':'text', 'label':'Ignored',      'name':'var'   },
-                    { 'type':'note', 'label':'start ${var}', 'name':'label' } ] },
-            xml__contains=[
-                '<label>start <output value=" /issue96_issue146/var "/></label>',
+                '<label><output value=" /issue96/var "/> text </label>',
             ])
