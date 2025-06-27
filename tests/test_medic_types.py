70d24cdadb3ab927939c34a7c7b5741ed80c8d39
Om Swastik Panda <omswastikpanda11@gmail.com>
2025-06-27 19:17:53 +0530

    feat: add support for Medic custom field types (#28)

---
+++ tests/test_medic_types.py
@@ -0,0 +1,68 @@
+# tests/test_custom_types_simple.py
+
+from tests.pyxform_test_case import PyxformTestCase
+from tests.xpath_helpers.questions import xpq
+
+
+class TestCustomTypesSimple(PyxformTestCase):
+    """
+    Markdown-only test for each of medic custom types.
+    """
+
+    def test_db_person_bind(self):
+        self.assertPyxformXform(
+            name="test_name",
+            md="""
+            | survey |               |           |
+            |        | type          | name      | label::en |
+            |        | db:person     | person    | Person    |
+            """,
+            # assert there exactly one <bind type="db:person" nodeset="/data/person">
+            xml__xpath_match=[xpq.model_instance_bind("person", "db:person")],
+        )
+
+    def test_db_clinic_bind(self):
+        self.assertPyxformXform(
+            name="test_name",
+            md="""
+            | survey |              |           |
+            |        | type         | name      | label::en |
+            |        | db:clinic    | clinic    | Clinic    |
+            """,
+            xml__xpath_match=[xpq.model_instance_bind("clinic", "db:clinic")],
+        )
+
+    def test_tel_bind(self):
+        self.assertPyxformXform(
+            name="test_name",
+            md="""
+            | survey |       |            |
+            |        | type  | name       | label::en    |
+            |        | tel   | phone      | Phone Number |
+            """,
+            xml__xpath_match=[xpq.model_instance_bind("phone", "tel")],
+        )
+
+    def test_db_health_center_bind(self):
+        self.assertPyxformXform(
+            name="test_name",
+            md="""
+            | survey |                  |           |
+            |        | type             | name      | label::en |
+            |        | db:health_center | health    | Health    |
+            """,
+            xml__xpath_match=[xpq.model_instance_bind("health", "db:health_center")],
+        )
+
+    def test_db_district_hospital_bind(self):
+        self.assertPyxformXform(
+            name="test_name",
+            md="""
+            | survey |                      |           |
+            |        | type                 | name      | label::en |
+            |        | db:district_hospital | district  | District  |
+            """,
+            xml__xpath_match=[
+                xpq.model_instance_bind("district", "db:district_hospital")
+            ],
+        )
